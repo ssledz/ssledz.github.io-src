ef0fb0fb92e151566e2024aff41205ac9924a0b7
Sławomir Śledź <slawomir.sledz@gmail.com>
2018-12-19 00:15:26 +0100

    playing with OptionT

--- monad-transformer/src/main/scala/pl/softech/learning/fp/OptionTExample.scala
+++ monad-transformer/src/main/scala/pl/softech/learning/fp/OptionTExample.scala
@@ -14,10 +14,22 @@ object OptionTExample extends App {
 
   def findAddressByUserId(userId: Long): Future[Option[Address]] = Future.successful(Some(Address(1L, "Aspekt")))
 
+  //  def findStreetByLogin(login: String): Function[String] =
+  //    for {
+  //      user <- findUserByLogin(login)
+  //      address <- findAddressByUserId(user.id)
+  //    } yield address.street
+
   //  val street = for {
   //    user <- findUserByLogin("ma")
   //    address <- findAddressByUserId(user.id)
   //  } yield address.street
+
+  //  def findStreetByLogin(login : String) : Function[Option[String]] =
+  //    for {
+  //      user <- findUserByLogin(login)
+  //      address <- findAddressByUserId(user.map(_.id).getOrElse(-1))
+  //    } yield address.map(_.street)
 
   val street = for {
     user <- OptionT(findUserByLogin("ss"))
