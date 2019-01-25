65b2d82e387dec20d010fdc25be361bdb946dbfc
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-01-25 22:27:43 +0100

    post...

--- monad-transformer/src/main/scala/pl/softech/learning/fp/ComposedMonad.scala
+++ monad-transformer/src/main/scala/pl/softech/learning/fp/ComposedMonad.scala
@@ -5,7 +5,27 @@ import scala.concurrent.{Await, Future}
 
 case class ComposedMonad[F[_], G[_], A](value: F[G[A]]) {
 
-  def flatMap[B](f: A => ComposedMonad[F, G, B])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] = ???
+//  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] = ComposedMonad[F, G, B] {
+//    F.flatMap(value) { ga : G[A] =>
+//      val gb : G[B] = G.flatMap(ga) { a : A =>
+//        val fgb : F[G[B]] = f(a)
+//        ???
+//      }
+//      F.pure(gb)
+//    }
+//  }
+
+  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] = ComposedMonad[F, G, B] {
+    F.flatMap(value) { ga : G[A] =>
+      val gfgb : G[F[G[B]]] = G.map(ga) { a : A =>
+        f(a)
+      }
+      ???
+    }
+  }
+
+  def flatMap[B](f: A => ComposedMonad[F, G, B])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] =
+    flatMapF(a => f(a).value)
 
   def map[B](f: A => B): ComposedMonad[F, G, B] = ???
 
