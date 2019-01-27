38628e20af2579ec8040341540ff5cb87854a830
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-01-28 00:14:23 +0100

    bzz

--- monad-transformer/src/main/scala/pl/softech/learning/fp/ComposedMonad.scala
+++ monad-transformer/src/main/scala/pl/softech/learning/fp/ComposedMonad.scala
@@ -3,45 +3,47 @@ package pl.softech.learning.fp
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-case class ComposedMonad[F[_], G[_], A](value: F[G[A]]) {
+case class CMonad[F[_], G[_], A](value: F[G[A]]) {
 
-//  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] = ComposedMonad[F, G, B] {
-//    F.flatMap(value) { ga : G[A] =>
-//      val gb : G[B] = G.flatMap(ga) { a : A =>
-//        val fgb : F[G[B]] = f(a)
-//        ???
-//      }
-//      F.pure(gb)
-//    }
-//  }
+  //  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): CMonad[F, G, B] =
+  //    CMonad[F, G, B] {
+  //      F.flatMap(value) { ga: G[A] =>
+  //        val gb: G[B] = G.flatMap(ga) { a: A =>
+  //          val fgb: F[G[B]] = f(a)
+  //          ???
+  //        }
+  //        F.pure(gb)
+  //      }
+  //    }
 
-  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] = ComposedMonad[F, G, B] {
-    F.flatMap(value) { ga : G[A] =>
-      val gfgb : G[F[G[B]]] = G.map(ga) { a : A =>
-        f(a)
+  def flatMapF[B](f: A => F[G[B]])(implicit F: Monad[F], G: Monad[G]): CMonad[F, G, B] =
+    CMonad[F, G, B] {
+      F.flatMap(value) { ga: G[A] =>
+        val gfgb: G[F[G[B]]] = G.map(ga) { a: A =>
+          f(a)
+        }
+        ???
       }
-      ???
     }
-  }
 
-  def flatMap[B](f: A => ComposedMonad[F, G, B])(implicit F: Monad[F], G: Monad[G]): ComposedMonad[F, G, B] =
+  def flatMap[B](f: A => CMonad[F, G, B])(implicit F: Monad[F], G: Monad[G]): CMonad[F, G, B] =
     flatMapF(a => f(a).value)
 
-  def map[B](f: A => B): ComposedMonad[F, G, B] = ???
+  def map[B](f: A => B): CMonad[F, G, B] = ???
 
 }
 
-object ComposedMonadExample extends App {
-
-  import OptionTExample._
+object CMonadExample extends App {
 
   import MonadInstances._
+  import OptionTExample._
+
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def findStreetByLogin(login: String): ComposedMonad[Future, Option, String] =
+  def findStreetByLogin(login: String): CMonad[Future, Option, String] =
     for {
-      user <- ComposedMonad(findUserByLogin(login))
-      address <- ComposedMonad(findAddressByUserId(user.id))
+      user <- CMonad(findUserByLogin(login))
+      address <- CMonad(findAddressByUserId(user.id))
     } yield address.street
 
   val res = Await.result(findStreetByLogin("ss").value, Duration.Inf)
