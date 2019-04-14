2902774c22a42112757dc2b02affae31b7bc103b
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-04-15 00:41:07 +0200

    playing with applicative & functor

--- monad-gentle-introduction/src/main/scala/applicative/OptionExample.scala
+++ monad-gentle-introduction/src/main/scala/applicative/OptionExample.scala
@@ -9,4 +9,10 @@ object OptionExample extends App {
   println((Option.pure(1), Option.pure(2)).mapN(_ + _))
   println((Option.empty[Int], Option.pure(2)).mapN(_ + _))
 
+  val add: Int => Int => Int = ((x: Int, y: Int) => (x + y)).curried
+  val liftedAdd: Option[Int => Int => Int] = Applicative[Option].pure(add)
+  val fbc: Option[Int => Int] = Applicative[Option].ap(liftedAdd)(Option.pure(1))
+  val fc = Applicative[Option].ap(fbc)(Option.pure(2))
+  println(fc)
+
 }
